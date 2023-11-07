c3cf5eecdae2a0462a93801cb64fc8b97f7051ad
Ronnachate <ronnachate@gmail.com>
2023-11-07 11:57:43 +0700

    iInclude swagger docs in apis.

--- apps/assistant-api/src/main.ts
+++ apps/assistant-api/src/main.ts
@@ -6,12 +6,23 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
+
+  const config = new DocumentBuilder()
+    .setTitle('Assistan api')
+    .setDescription('Api providing assitant information')
+    .setVersion('0.1')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   const port = process.env.PORT || 3001;
   await app.listen(port);
   Logger.log(
--- apps/message-api/src/main.ts
+++ apps/message-api/src/main.ts
@@ -7,11 +7,21 @@ import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
+
+  const config = new DocumentBuilder()
+    .setTitle('Message api')
+    .setDescription('Api providing assitant messages')
+    .setVersion('0.1')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   const port = process.env.PORT || 3002;
   await app.listen(port);
   Logger.log(
