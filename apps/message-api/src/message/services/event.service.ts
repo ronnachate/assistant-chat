be4ec8064f45507c923fa6b868dd8380b61e0f1b
Ronnachate <ronnachate@gmail.com>
2023-11-08 20:00:30 +0700

    Create new assistan record when assistantEvent hooked.

--- apps/message-api/src/message/services/event.service.ts
+++ apps/message-api/src/message/services/event.service.ts
@@ -1,28 +1,36 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { SeqLogger } from '@jasonsoft/nestjs-seq';
-import { RabbitRPC } from '@golevelup/nestjs-rabbitmq';
+import { Nack, RabbitRPC } from '@golevelup/nestjs-rabbitmq';
 import { RABBITMQ_ASSISTANT_CREATE_ROUTING } from '@assistant-chat/constants';
 import { AssistantDTO } from '@assistant-chat/dtos';
+import { AssistantRepository } from '../repositories/assistant.repository';
 
 @Injectable()
 export class EventService {
-
-  
   constructor(
-    private readonly configService: ConfigService,
+    private readonly assistantRepository: AssistantRepository,
     private readonly logger: SeqLogger
-  ) {
-  }
+  ) {}
 
   @RabbitRPC({
     exchange: process.env.RABBITMQ_EXCHANGE_NAME,
     routingKey: RABBITMQ_ASSISTANT_CREATE_ROUTING,
   })
-  public async assistantCreatedHandler(assistantDto: {}) {
-    this.logger.info('assistantCreatedHandler recieved', { assistantDto });
-    return {
-      response: 42,
-    };
+  public async assistantCreatedHandler(assistantDto: AssistantDTO) {
+    try {
+      this.logger.info('assistantCreatedHandler recieved', { assistantDto });
+      var newAssistant = {
+        assistantID: assistantDto.id,
+      };
+      await this.assistantRepository.create(newAssistant);
+      return assistantDto;
+    } catch (err) {
+      this.logger.error('assistantCreatedHandler error', {
+        module: EventService.name,
+        error: err,
+      });
+      throw err;
+    }
   }
 }
